a73478c4d4e69d0256a31adacb67acea1adf1a94
Liubou Liaukovich <levkovich.ln@gmail.com>
2021-01-25 00:24:07 +0300

    add1

---
+++ src/lesson4/additional1.java
@@ -0,0 +1,24 @@
+package lesson4;
+
+import java.util.Arrays;
+
+public class additional1 {
+    public static void main(String[] args) {
+        int a = -115;
+
+        if (a >= 1 & a <= 9) System.out.println(a + " is a one-digit positive number");
+        else if (a == -1 & a >= -9) System.out.println(a + " is a one-digit negative number");
+
+        if (a >= 10 & a < 100) System.out.println(a + " is a two-digit positive number");
+        else if (a <= -10 & a > -100) System.out.println(a + " is a two-digit negative number");
+
+        if (a >= 100 & a < 1000) System.out.println(a + " is a three-digit positive number");
+        else if (a <= -100 & a > -1000) System.out.println(a + " is a three-digit negative number");
+
+        else if (a == 0) System.out.println(a + " is a one-digit integer");
+    }
+}
+//В переменную записываем число.
+// Надо вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
+// Например, "это однозначное положительное число".
+// Достаточно будет определить, является ли число однозначным, "двузначным или трехзначным и более.
