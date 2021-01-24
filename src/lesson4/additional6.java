745fe82371d4058996b73f11d83690dd24e4a481
Liubou Liaukovich <levkovich.ln@gmail.com>
2021-01-25 00:49:39 +0300

    add6

---
+++ src/lesson4/additional6.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+import java.util.Arrays;
+
+public class additional6 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 112;
+        if (a >= b) System.out.println(a);
+        else System.out.println(b);
+
+    }
+}
+//Даны 2 числа. Вывести большее из них
