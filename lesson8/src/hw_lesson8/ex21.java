70241754806bc2876a0ce57a01091731fa64506a
Liubou Liaukovich <levkovich.ln@gmail.com>
2021-02-21 11:19:18 +0300

    HW 8 2nd fix

--- lesson8/src/hw_lesson8/ex21.java
+++ lesson8/src/hw_lesson8/ex21.java
@@ -6,27 +6,27 @@ public class ex21 {
     public static void main(String[] args) {
             long startTime = System.currentTimeMillis();
             String str = " To be, or   not to be, that is the question: Whether 'tis nobler in the mind to suffer";
-            str += "The slings and arrows";
-            str += "of outrageous fortune,";
-            str += "Or to take arms against a sea of troubles";
+            for (int i = 0; i < 1000; i++) {
+                    str += "The slings and arrows";
+            };
             System.out.println(str);
             System.out.println("That took " + (System.currentTimeMillis()-startTime) + " milliseconds");
 
 
             long startTimeBuilder = System.currentTimeMillis();
             StringBuilder str2 = new StringBuilder("To be, or   not to be, that is the question: Whether 'tis nobler in the mind to sufferThe slings and arrowsof outrageous fortune,Or to take arms against a sea of troubles");
-            str2.append("and by opposing end them");
-            str2.append("To die—to sleep, No more; and by a sleep ");
-            str2.append("to say we end The heart-ache and the thousand natural");
+            for (int i = 0; i < 1000; i++) {
+                    str2.append("and by opposing end them");
+            }
             System.out.println(str2);
             System.out.println("That took " + (System.currentTimeMillis() - startTimeBuilder) + " milliseconds");
 
 
             long startTimeBuffer = System.currentTimeMillis();
             StringBuffer str3 = new StringBuffer("To be, or   not to be, that is the question: Whether 'tis nobler in the mind to sufferThe slings and arrowsof outrageous fortune,Or to take arms against a sea of troubles");
-            str3.append("and by opposing end them");
-            str3.append("To die—to sleep, No more; and by a sleep ");
-            str3.append("to say we end The heart-ache and the thousand natural");
+            for (int i = 0; i < 1000; i++) {
+                    str3.append("and by opposing end them");
+            }
             System.out.println(str3);
             System.out.println("That took " + (System.currentTimeMillis() - startTimeBuffer) + " milliseconds");
 
